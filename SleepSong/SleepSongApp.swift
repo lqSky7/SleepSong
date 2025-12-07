5f387cc296b60e69d2931d194026499c8f75a2e4
ca5 <catinice@outlook.com>
2025-12-07 11:09:07 +0530

    Initial Commit

---
+++ SleepSong/SleepSongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepSongApp.swift
+//  SleepSong
+//
+//  Created by ca5 on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepSongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
