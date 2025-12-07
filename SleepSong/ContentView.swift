c598d11f630c5ab3a186ca6b174cb601f1e28235
ca5 <catinice@outlook.com>
2025-12-07 13:24:27 +0530

    init impl of app
    Signed-off-by: ca5 <catinice@outlook.com>

--- SleepSong/ContentView.swift
+++ SleepSong/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var sleepHours : Double = 8.0
-    @State var dateToday : Date = Date.now
-    var body: some View {
-        Stepper("Sleep Hours \(sleepHours.formatted())", value: $sleepHours, in: 2...16, step: 0.5)
-            .labelsHidden()
-        DatePicker("Pick a date", selection: $dateToday,
-                   in: Date.now...,
-                   displayedComponents: .hourAndMinute).labelsHidden()
-    }
-    func exp(){
-        var comps : DateComponents = DateComponents()
-        comps.hour = 5
-        comps.minute = Calendar.current.component(.minute, from: Date.now)
-        let userSetDate = Calendar.current.date(from: comps) ?? .now
+    @State private var wakeUp = Date.now
+    @State private var coffee = 2
+    @State private var SleepAmount = 8.0
+    var body : some View{
+        NavigationStack{
+            VStack(spacing: 20){
+                Text("Your wake up time?").font(.headline).bold()
+                DatePicker("Select wakeup time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                Text("Desired amount of sleep?").font(.headline).bold()
+                Stepper("\(SleepAmount.formatted()) Hours", value: $SleepAmount, in: 4...14, step: 0.5).padding(.horizontal, 40)
+                Text("Coffee per day?").font(.headline).bold()
+                Stepper("\(coffee.formatted()) Cups", value: $coffee, in: 0...10).padding(.horizontal, 40)
+                
+            }.navigationTitle("SleepSong").navigationBarTitleDisplayMode(.automatic)
+        }
     }
 }
 
